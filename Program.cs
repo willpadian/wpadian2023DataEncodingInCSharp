28f2a9c47e7a729da96c36addc04087fdb1291ad
willpadian <wpadian2023@chadwickschool.org>
2022-01-13 12:06:46 -0800

    chore: Finished first part of activity

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace wpadian2023DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char ch;
+            ch = 'A';
+            Console.WriteLine($"ch is storing the value: {ch}");
+
+            int ch_decimal;
+            ch_decimal = ch;
+            Console.WriteLine($"The decimal value of ch is: {ch_decimal}");
+
+            int toConvert;
+            Console.WriteLine("Enter an integer: ");
+            toConvert = int.Parse(Console.ReadLine());
+
+            char asChar;
+            asChar = (char)toConvert;
+            Console.WriteLine($"The integer {toConvert} cast to a char is '{asChar}'");
+
+            char b;
+            b = (char)('A' + 1);
+            Console.WriteLine($"'A' + 1 = '{b}'");
         }
     }
 }
