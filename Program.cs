0fb0a81eba921a22a91ba4160f2bfcc2e5f4870c
willpadian <wpadian2023@chadwickschool.org>
2022-01-13 11:46:43 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wpadian2023DataEncodingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
